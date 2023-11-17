1ada0a5d1e402969af5873da424c9be2e8b8bea8
Diangien <josenkana13@gmail.com>
2023-11-17 17:07:44 -0300

    n

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 let txt = document.querySelector('#txtTask');
 let btn = document.querySelector('#btnAdd');
 let container = document.querySelector('#tasks');
+let btnSalvar = document.getElementById('save')
 let audio = new Audio('audio/pato.mp3')
+let salvou = false
 
 let tarefas = []
+
 let divTarefasArray = []
 
-container.style.display = 'none';
+imprimirDefualt()
+
+btnSalvar.addEventListener('change',(evt)=>{
+    if(btnSalvar.checked){
+        salvou = btnSalvar.checked = false
+        localStorage.clear()
+    }else{
+        salvou = btnSalvar.checked = true
+        salvarTarefas()
+    }
+})
 
 document.addEventListener('keydown',(evt)=>{
     let tecla = evt.key
@@ -25,18 +38,75 @@ btn.addEventListener('click',()=>{
 
 let adicionarTarefa = text =>{
 
-   
-    container.style.display = 'block'
     if(tarefas.includes(text) | txt.value=='' | divTarefasArray.length > 13){
         return
     }
 
+    container.style.display = 'block'
+
     txt.value = ''
     tarefas.push(text)
+    criarElemento(text)
+    imprimir()
+
+    console.log(tarefas)
+    
+}
+
+function salvarTarefas(){
+    let tarefasAntigas = []
+
+    if(localStorage.getItem("tarefas")){
+        if(localStorage.length == 0){
+            return;
+        }
+        tarefasAntigas = localStorage.getItem("tarefas").split(',')
+    }
+
+    tarefas = tarefas.concat(tarefasAntigas)
+        
+    
+    localStorage.setItem("tarefas",tarefas)
+}
+
+function imprimir(){
+    
+    if(divTarefasArray.length == 0){
+        container.style.display = 'none'
+        return;
+    }
+
+    divTarefasArray.map(el=>{
+        container.appendChild(el)
+    })
+
+}
+
+let eliminar = (elemento)=>{
+
+    container.innerHTML = ''
+
+    divTarefasArray = divTarefasArray.filter((el)=>{
+        return el.dataset.tarefa != elemento.dataset.nome
+    })
+
+    tarefas = tarefas.filter((el)=>{
+        return el != elemento.dataset.nome
+    })
 
     
+    
+    if(tarefas.length !=0)
+        localStorage.setItem("tarefas",tarefas)
+    else
+        localStorage.clear()
 
-    //Div Tarefa
+    imprimir()
+
+}
+
+function criarElemento(text){
+
     let divTarefa = document.createElement('div')
     divTarefa.setAttribute("data-tarefa",text)
     divTarefa.setAttribute("class","tarefa")
@@ -61,8 +131,6 @@ let adicionarTarefa = text =>{
 
     let btnEliminar = document.createElement('img')
     btnEliminar.src = './images/lixeira12.png'
-   //btnEliminar.innerHTML = 'x'
-    //btnEliminar.style.color = '#f00'
     btnEliminar.style.width = '40px';
 
     btnEliminar.style.cursor = 'pointer'
@@ -71,7 +139,6 @@ let adicionarTarefa = text =>{
          eliminar(evt.target)
     })
 
-    console.log(tarefas)
 
     
     divTarefa.appendChild(btnCompletar)
@@ -79,34 +146,30 @@ let adicionarTarefa = text =>{
     divTarefa.appendChild(btnEliminar)
 
     divTarefasArray.push(divTarefa)
-
-
-    imprimir()
-    
 }
 
-let imprimir = ()=>{
-    
-    if(divTarefasArray.length == 0){
-        container.style.display = 'none'
-        return;
+function imprimirDefualt(){
+    if(localStorage.length != 0){
+
+        let arrayTarefas = localStorage.getItem("tarefas").split(',')
+
+        arrayTarefas.map((el)=>{
+            criarElemento(el)
+        })
+
+        console.log("cheguei")
+        
+        tarefas = tarefas.concat(arrayTarefas)
+
+        divTarefasArray.map(el=>{
+            container.appendChild(el)
+            console.log("appendeu")
+        })
+    }else{
+        container.style.display = 'none';
     }
-
-
-    divTarefasArray.map(el=>{
-        container.appendChild(el)
-    })
-
 }
 
-let eliminar = (elemento)=>{
 
-    container.innerHTML = ''
+console.log(localStorage.getItem("tarefas"))
 
-    divTarefasArray = divTarefasArray.filter((el)=>{
-        return el.dataset.tarefa != elemento.dataset.nome
-    })
-
-    imprimir()
-
-}
